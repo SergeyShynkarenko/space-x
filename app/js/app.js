2e037ea89b4a7c0da9df52ba5ba4e243a4082428
SergeyShynkarenko <serg.shunkarenko@gmail.com>
2021-12-02 12:32:52 +0200

    add footer

--- app/js/app.js
+++ app/js/app.js
@@ -6,9 +6,19 @@ import $ from 'jquery'
 // // Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
 
-require('./vendor/slick-slider/slick.min.js')
-document.addEventListener('DOMContentLoaded', () => {
+require('../js/vendor/slick-slider/slick.min.js')
+	document.addEventListener('DOMContentLoaded', () => {
 
+	document.querySelector('.hamburger').addEventListener('click', function () {
+		document.querySelector('.hamburger--slider').classList.toggle('is-active');
+		document.querySelector('.menu__list').classList.toggle('menu__list--active');
+	});
 
+  $(function() {
+    $('.slider__box').slick({
+    	prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/dist/arrow-left.svg" alt="Arrow-left">',
+    	nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/dist/arrow-right.svg" alt="Arrow-right">',
+    });
+	});
 
-})
+});
